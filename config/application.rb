afe58f5a5ba0c178ddda159d62dc99c34b788979
Bruce <brucepark0@gmail.com>
2017-02-18 17:53:11 -0800

    Configure to api_only
    - Remove unnecessary gems

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,7 @@ module Rails5ApiOnly
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.api_only = true
   end
 end
